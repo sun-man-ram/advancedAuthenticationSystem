8023128f33da040b8f07565790afec699f891138
sun-man-ram <manikantasunkara2653@gmail.com>
2025-06-30 23:10:45 +0530

    second commit

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,16 @@
 import express from "express";
-
+import dotenv from "dotenv";
+import { connectDB } from "./db/connectDB.js";
+import authRoutes from "./routes/auth.route.js";
+dotenv.config();
 const app=express();
-
+const PORT = process.env.PORT || 5000;
 app.get("/",(req,res)=>{
     res.send("hello world 1234");
 });
-app.listen(3000,()=>{
+app.use("/api/auth",authRoutes);
+app.listen(5000,()=>{
+    connectDB();
     console.log("Server is running on port 3000");
 });
+
